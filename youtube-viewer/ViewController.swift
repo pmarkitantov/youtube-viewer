bcf6011357cf3df1353c30ffac7b3324b4782e99
Pavel Markitantov <pmarkitantov@gmail.com>
2025-01-15 18:06:33 +0100

    initial xcode project setup

---
+++ youtube-viewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-viewer
+//
+//  Created by Pavel Markitantov on 15/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
